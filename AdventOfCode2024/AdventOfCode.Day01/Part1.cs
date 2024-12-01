4d5d118a03c2e43a8e377e9cd6c18386d126cd8b
Tomas Bruckner <tomasbrucknermail@gmail.com>
2024-12-01 10:47:59 +0100

    day01

---
+++ AdventOfCode2024/AdventOfCode.Day01/Part1.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Day01;
+
+public static partial class Part1
+{
+    public static long Solve(string input)
+    {
+        var result = 0L;
+        var lines = input.Split(Environment.NewLine);
+        var arr1 = new List<long>();
+        var arr2 = new List<long>();
+
+        foreach (var line in lines)
+        {
+            var parts = MyRegex().Match(line);
+            if (!parts.Success)
+            {
+                continue;
+            }
+            
+            arr1.Add(long.Parse(parts.Groups[1].Value));
+            arr2.Add(long.Parse(parts.Groups[2].Value));
+        }
+        
+        arr1 = arr1.OrderBy(x => x).ToList();
+        arr2 = arr2.OrderBy(x => x).ToList();
+
+        for (var i = 0; i < arr1.Count; i++)
+        {
+            result += Math.Abs(arr1[i] - arr2[i]);
+        }
+
+        return result;
+    }
+
+    [GeneratedRegex(@"^(\d+) +(\d+)")]
+    private static partial Regex MyRegex();
+}
---
+++ AdventOfCode2024/AdventOfCode.Day01/Part2.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Day01;
+
+public static partial class Part2
+{
+    public static long Solve(string input)
+    {
+        var result = 0L;
+        var lines = input.Split(Environment.NewLine);
+        var arr1 = new List<long>();
+        var countMap = new Dictionary<long, long>();
+
+        foreach (var line in lines)
+        {
+            var parts = MyRegex().Match(line);
+            if (!parts.Success)
+            {
+                continue;
+            }
+            
+            arr1.Add(long.Parse(parts.Groups[1].Value));
+            var num2 = long.Parse(parts.Groups[2].Value);
+            if (!countMap.TryAdd(num2, 1))
+            {
+                countMap[num2] += 1;
+            }
+        }
+        
+        arr1 = arr1.OrderBy(x => x).ToList();
+
+        foreach (var t in arr1)
+        {
+            if (countMap.TryGetValue(t, out var value))
+            {
+                result += t * value;
+            }
+        }
+
+        return result;
+    }
+
+    [GeneratedRegex(@"^(\d+) +(\d+)")]
+    private static partial Regex MyRegex();
+}
