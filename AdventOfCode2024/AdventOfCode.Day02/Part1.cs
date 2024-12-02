d1466ecab28aa15397a4fd173e125916130207a8
Tomas Bruckner <tomasbrucknermail@gmail.com>
2024-12-02 07:32:31 +0100

    day2

---
+++ AdventOfCode2024/AdventOfCode.Day02/Part1.cs
@@ -0,0 +1,47 @@
+﻿namespace AdventOfCode.Day02;
+
+public static class Part1
+{
+    public static long Solve(string input)
+    {
+        var lines = input.Split(Environment.NewLine);
+
+        var result = lines.LongCount(IsValid);
+
+        return result;
+    }
+
+    private static bool IsValid(string line)
+    {
+        long? previous = null;
+        Direction? direction = null;
+
+        foreach (var num in line.Split(' '))
+        {
+            var current = long.Parse(num);
+            if (!previous.HasValue)
+            {
+                previous = current;
+                continue;
+            }
+
+            direction ??= previous > current ? Direction.Decreasing : Direction.Increasing;
+
+            var diff = direction == Direction.Decreasing ? previous.Value - current : current - previous.Value;
+            if (diff is < 1 or > 3)
+            {
+                return false;
+            }
+            
+            previous = current;
+        }
+
+        return true;
+    }
+
+    private enum Direction
+    {
+        Increasing,
+        Decreasing,
+    }
+}
---
+++ AdventOfCode2024/AdventOfCode.Day02/Part2.cs
@@ -0,0 +1,57 @@
+﻿namespace AdventOfCode.Day02;
+
+public static class Part2
+{
+    public static long Solve(string input)
+    {
+        var lines = input.Split(Environment.NewLine);
+        var result = lines.LongCount(x =>
+        {
+            var nums = x.Split(" ")
+                .Select(long.Parse)
+                .ToList();
+            return IsValid(nums);
+        });
+
+        return result;
+    }
+
+    private static bool IsValid(List<long> nums)
+    {
+        long? previous = null;
+        Direction? direction = null;
+        var allowErrors = true;
+
+        foreach (var current in nums)
+        {
+            if (!previous.HasValue)
+            {
+                previous = current;
+                continue;
+            }
+
+            direction ??= previous > current ? Direction.Decreasing : Direction.Increasing;
+
+            var diff = direction == Direction.Decreasing ? previous.Value - current : current - previous.Value;
+            if (diff is < 1 or > 3)
+            {
+                if (!allowErrors)
+                {
+                    return false;
+                }
+
+                allowErrors = false;
+            }
+
+            previous = current;
+        }
+
+        return true;
+    }
+
+    private enum Direction
+    {
+        Increasing,
+        Decreasing,
+    }
+}
