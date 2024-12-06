8568208d1a35d3e95a7d64b6f26e83be76ee93a5
Tomas Bruckner <tomasbrucknermail@gmail.com>
2024-12-06 12:35:33 +0100

    day 6

---
+++ AdventOfCode2024/AdventOfCode.Day06/Part1.cs
@@ -0,0 +1,107 @@
+﻿namespace AdventOfCode.Day06;
+
+public static class Part1
+{
+    private enum DirectionEnum
+    {
+        Up,
+        Right,
+        Left,
+        Down,
+        Finished,
+    }
+
+    private static readonly Dictionary<DirectionEnum, (int x, int y)> Directions = new()
+    {
+        { DirectionEnum.Up, (0, -1) },
+        { DirectionEnum.Right, (1, 0) },
+        { DirectionEnum.Left, (-1, 0) },
+        { DirectionEnum.Down, (0, 1) },
+    };
+
+    private static readonly Dictionary<char, DirectionEnum> DirectionMap = new()
+    {
+        { '^', DirectionEnum.Up },
+        { '>', DirectionEnum.Right },
+        { '<', DirectionEnum.Left },
+        { 'v', DirectionEnum.Down },
+    };
+
+    private static readonly Dictionary<DirectionEnum, DirectionEnum> DirectionRotationMap = new()
+    {
+        { DirectionEnum.Left, DirectionEnum.Up },
+        { DirectionEnum.Up, DirectionEnum.Right },
+        { DirectionEnum.Right, DirectionEnum.Down },
+        { DirectionEnum.Down, DirectionEnum.Left },
+    };
+
+    public static long Solve(string input)
+    {
+        var matrix = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.ToCharArray().ToList())
+            .ToList();
+        var result = GetResult(matrix);
+
+        return result;
+    }
+
+    private static long GetResult(List<List<char>> matrix)
+    {
+        var visited = new HashSet<(int, int)>();
+        var (x, y, direction) = FindStart(matrix);
+        matrix[y][x] = '.';
+        visited.Add((x, y));
+
+        do
+        {
+            var (xOffset, yOffset) = Directions[direction];
+            var newDirection = GetNewDirection(matrix, x + xOffset, y + yOffset, direction);
+            if (newDirection == DirectionEnum.Finished)
+            {
+                return visited.Count;
+            }
+
+            if (newDirection != direction)
+            {
+                direction = newDirection;
+                continue;
+            }
+
+            x += Directions[direction].x;
+            y += Directions[direction].y;
+
+            visited.Add((x, y));
+        } while (true);
+    }
+
+    private static (int x, int y, DirectionEnum direction) FindStart(List<List<char>> matrix)
+    {
+        for (var y = 0; y < matrix.Count; y++)
+        {
+            for (var x = 0; x < matrix[y].Count; x++)
+            {
+                if (DirectionMap.ContainsKey(matrix[y][x]))
+                {
+                    return (x, y, DirectionMap[matrix[y][x]]);
+                }
+            }
+        }
+
+        throw new Exception("Start not found");
+    }
+
+    private static DirectionEnum GetNewDirection(List<List<char>> matrix, int x, int y, DirectionEnum direction)
+    {
+        if (OutOfRange(matrix, x, y))
+        {
+            return DirectionEnum.Finished;
+        }
+
+        return matrix[y][x] == '.' ? direction : DirectionRotationMap[direction];
+    }
+
+    private static bool OutOfRange(List<List<char>> matrix, int x, int y)
+    {
+        return y > matrix.Count - 1 || y < 0 || x < 0 || x > matrix[y].Count - 1;
+    }
+}
